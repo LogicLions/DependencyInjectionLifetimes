ca490cf4fdb7cef1f5ce48f628eb219e9042c62b
LogicLions <sachinyt2024@gmail.com>
2024-11-30 21:08:03 +0530

    Modify comments

--- DependencyInjectionLifetimesUsingMVC/Controllers/HomeController.cs
+++ DependencyInjectionLifetimesUsingMVC/Controllers/HomeController.cs
@@ -50,7 +50,7 @@ namespace DependencyInjectionLifetimesUsingMVC.Controllers
             // for every refresh ...
             // Transient : 2 objects will generate new guids everytime seperately. means 1st object is not shared with 2nd request. it creates new object.
             // Scoped : 2 objects will generate same guids. guid changes only when page refreshed. but same for both objects. means 1st object is shared for 2nd request.
-            // Singleton : 2 objects will generate same guids. same guids even if page refreshed. means one single object is shared for entire application lifetime.
+            // Singleton : 2 objects will generate same guids. same guids even if page refreshed. means one single object is shared for entire application lifetime..
 
             return Ok(messages.ToString());
         }
